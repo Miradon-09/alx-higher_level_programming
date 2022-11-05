854070c90168552e584909a53552f22ae98e2552
Ayoade micheal <ayoademic03@gmail.com>
2022-11-05 00:59:35 +0000

    Projects on more classes.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 2020
+@author: Robinson Montes
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
